332c9cfc9a6138c44f87da11a400674a13021ee1
김예안 <kjan0929@naver.com>
2026-03-05 20:05:59 +0900

    test(Profit): 수익률 계산 테스트 작성
    Co-authored-by: Untae Lee <bigcloud07@users.noreply.github.com>

---
+++ __tests__/ProfitTest.js
@@ -0,0 +1,28 @@
+import { LOTTO_RULE } from "../src/constants.js";
+import Profit from "../src/Profit.js";
+
+// TODO: 공통 유틸로 분리
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(console, "log");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+test("수익률 계산 test", () => {
+  const logSpy = getLogSpy();
+  const purchasedAmount = 1000;
+
+  const stats = {
+    [LOTTO_RULE["3_MATCH"]]: { count: 2, prize: 5000 },
+    [LOTTO_RULE["4_MATCH"]]: { count: 0, prize: 50000 },
+    [LOTTO_RULE["5_MATCH"]]: { count: 0, prize: 1500000 },
+    [LOTTO_RULE["5_BONUS_MATCH"]]: { count: 0, prize: 30000000 },
+    [LOTTO_RULE["6_MATCH"]]: { count: 0, prize: 2000000000 },
+  };
+
+  Profit.printProfitRate(purchasedAmount, stats);
+
+  expect(logSpy).toHaveBeenCalledWith(
+    expect.stringContaining(`총 수익률은 1000.0%입니다.`)
+  );
+});
