d1a20d5d1f555a694555434aca3c6d5de0927e56
김예안 <kjan0929@naver.com>
2026-03-06 14:47:15 +0900

    fix(test): 입력 테스트 코드 비동기 실행 종료 로직 추가
    Co-authored-by: Untae Lee <bigcloud07@users.noreply.github.com>

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -1,14 +1,24 @@
-test('사용자가 8000을 입력하면 "8000"을 반환해야 한다.', async () => {
+import InputView from "../src/InputView.js";
+
+describe("입력 기능 test", () => {
+  afterAll(() => {
+    InputView.close();
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
-  const readSpy = jest
-    .spyOn(InputView, "readPurchaseAmount")
-    .mockResolvedValueOnce("8000");
-  const result = await InputView.readPurchaseAmount(
-    "구입금액을 입력해 주세요.\n"
-  );
 
-  expect(result).toBe("8000");
-  expect(readSpy).toHaveBeenCalledWith("구입금액을 입력해 주세요.\n");
+  test('사용자가 8000을 입력하면 "8000"을 반환해야 한다.', async () => {
+    const readSpy = jest
+      .spyOn(InputView, "readPurchaseAmount")
+      .mockResolvedValueOnce("8000");
+
+    const result = await InputView.readPurchaseAmount(
+      "구입금액을 입력해 주세요.\n"
+    );
+
+    expect(result).toBe("8000");
+    expect(readSpy).toHaveBeenCalledWith("구입금액을 입력해 주세요.\n");
+  });
 });
