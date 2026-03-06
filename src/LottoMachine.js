1a3b88e1e07bcb2ae922ba5da2bbedab7d7c5eaf
김예안 <kjan0929@naver.com>
2026-03-06 16:55:33 +0900

    feat(LottoMachine): 로또 발행 기능 구현
    Co-authored-by: Untae Lee <bigcloud07@users.noreply.github.com>

---
+++ src/LottoMachine.js
@@ -0,0 +1,41 @@
+import { LOTTO_RULE } from "./constants.js";
+import Lotto from "./Lotto.js";
+
+export default class LottoMachine {
+  constructor(purchaseAmount) {
+    this.lottoCount = purchaseAmount / LOTTO_RULE.LOTTO_PRICE;
+    this.lottos = [];
+  }
+
+  generateLottoNumbers(min, max, count) {
+    const lottoNumbers = new Set();
+
+    while (lottoNumbers.size < count) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      lottoNumbers.add(randomNumber);
+    }
+
+    return Array.from(lottoNumbers).sort((a, b) => a - b);
+  }
+
+  issuedLottos() {
+    Array.from({ length: this.lottoCount }).forEach(() => {
+      const lottoNumbers = this.generateLottoNumbers(1, 45, 6);
+      const lotto = new Lotto(lottoNumbers);
+      this.lottos.push(lotto);
+    });
+  }
+
+  printIssuedLottos() {
+    this.lottos.forEach((lotto) => {
+      console.log(lotto.getNumbers());
+    });
+  }
+
+  run() {
+    this.issuedLottos();
+    this.printIssuedLottos();
+
+    return this.lottos;
+  }
+}
