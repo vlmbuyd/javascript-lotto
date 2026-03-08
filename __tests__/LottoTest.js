ec17f21e5eab5ceaeabf958cc260b5ac8a51de69
김예안 <kjan0929@naver.com>
2026-03-08 13:27:56 +0900

    test(Lotto): 로또 유효성 검사 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -30,3 +30,17 @@ describe("당첨 번호 및 보너스 번호 비교 test", () => {
     ).toBe(6);
   });
 });
+
+describe("로또 유효성 검증 test", () => {
+  test("로또 번호 개수가 6개가 아닌 경우", () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5, 6, 7])).toThrow("[ERROR]");
+  });
+
+  test("중복된 번호가 포함된 경우", () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5, 5])).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 1~45 범위를 벗어난 경우", () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5, 46])).toThrow("[ERROR]");
+  });
+});
