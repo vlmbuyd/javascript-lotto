27cfcb8cb6ab155c70c2735bf70e06d9573a0124
김예안 <kjan0929@naver.com>
2026-03-08 14:15:48 +0900

    refactor(test): 당첨 통계 출력 단위 테스트 로직 개선
    logSpy, lottos를 매 단위 테스트마다 초기화

--- __tests__/RankTest.js
+++ __tests__/RankTest.js
@@ -11,16 +11,24 @@ const getLogSpy = () => {
 
 describe("당첨 통계 출력 test", () => {
   const logSpy = getLogSpy();
-  const lottos = [
-    new Lotto([8, 21, 23, 41, 42, 43]),
-    new Lotto([3, 5, 11, 16, 32, 38]),
-    new Lotto([7, 11, 16, 35, 36, 44]),
-    new Lotto([1, 8, 11, 31, 41, 42]),
-    new Lotto([13, 14, 16, 38, 42, 45]),
-    new Lotto([7, 11, 30, 40, 42, 43]),
-    new Lotto([2, 13, 22, 32, 38, 45]),
-    new Lotto([1, 2, 3, 14, 22, 45]),
-  ];
+  let lottos;
+
+  beforeEach(() => {
+    lottos = [
+      new Lotto([8, 21, 23, 41, 42, 43]),
+      new Lotto([3, 5, 11, 16, 32, 38]),
+      new Lotto([7, 11, 16, 35, 36, 44]),
+      new Lotto([1, 8, 11, 31, 41, 42]),
+      new Lotto([13, 14, 16, 38, 42, 45]),
+      new Lotto([7, 11, 30, 40, 42, 43]),
+      new Lotto([2, 13, 22, 32, 38, 45]),
+      new Lotto([1, 2, 3, 14, 22, 45]),
+    ];
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   test.each([
     [
